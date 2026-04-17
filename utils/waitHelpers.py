cbf1b60168ffac8bda006eff85ef78e16f66c04b
Tutorial <foditoon1@gmail.com>
2026-04-17 14:51:53 +0400

    Создание базовой страницы для добавления общих методов работы с элементами страниц
    Файл urls - хранение основных URL сайта
    
    waitHelpers - Класс с методами ожидания, вынесет отдельно для переиспользования в разные тестах

--- utils/waitHelpers.py
+++ utils/waitHelpers.py
@@ -3,10 +3,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
 class WaitHelpers:
+    """
+    Класс утилитов ожидания элементов
+    """
     def __init__(self, driver):
         self.driver = driver
         
     def wait_for_element(self, locator, timeout=10):
+        """Метод ожидания видимости элемента"""
         try:
             element = WebDriverWait(self.driver, timeout).until(
                 EC.presence_of_element_located(locator)
@@ -17,6 +21,7 @@ class WaitHelpers:
             return None
         
     def wait_for_clickable(self, locator, timeout=10):
+        """Метод проверки кликабельности элемента"""
         try:
             element = WebDriverWait(self.driver, timeout).until(
                 EC.element_to_be_clickable(locator)
